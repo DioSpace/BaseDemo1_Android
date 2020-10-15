34f029d31b2dd5485a0c94ede32b96e49bdb3eaa
DioSpace <zhendong2011@live.cn>
2020-10-16 04:17:57 +0800

    改进
    用反射实现json中的方法和Handle里的方法映射

--- app/src/main/java/com/my/basedemo1/MainActivity.java
+++ app/src/main/java/com/my/basedemo1/MainActivity.java
@@ -14,74 +14,42 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.my.basedemo1.model.JsonRootBean;
+import com.my.basedemo1.model.List_info;
+import com.my.basedemo1.util.GetJsonDataUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView show_board;
-    final static String[] data = {
-            "func_1", "func_2", "func_3", "func_4", "func_5", "func_6",
-            "func_7", "func_8", "func_9", "func_10", "func_11", "func_12"
-    };
-
-    private void func_1(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_2(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_3(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_4(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_5(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_6(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_7(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_8(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_9(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_10(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_11(String param) {
-        show_board.setText(param);
-    }
-
-    private void func_12(String param) {
-        show_board.setText(param);
-    }
-
+    JsonRootBean jsonRootBean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        show_board = (TextView) findViewById(R.id.show_board_id);
+        show_board = findViewById(R.id.show_board_id);
 
-        ListView listView = (ListView) findViewById(R.id.lv_main);
+        String json_str = GetJsonDataUtil.getJson(this, "list.json");
+        Gson gson = new Gson();
+        jsonRootBean = gson.fromJson(json_str, JsonRootBean.class);
+
+        //1.获取ListView对象
+        ListView listView = findViewById(R.id.lv_main);
+        //2.准备数据
+        String[] func_name = new String[jsonRootBean.getList_info().size()];
+        for (int i = 0; i < jsonRootBean.getList_info().size(); i++) {
+            func_name[i] = jsonRootBean.getList_info().get(i).getButton_name();
+        }
+        //3.准备适配器Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, //context 上下文
                 android.R.layout.simple_list_item_1, //行布局:系统自带的布局
-                data//数据源
+                func_name//数据源
         ) {
             @Override
             public boolean isEnabled(int position) {
@@ -91,58 +59,37 @@ public class MainActivity extends AppCompatActivity {
             @NonNull
             @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                ////重载该方法，在这个方法中，将每个Item的Gravity设置为CENTER
+                //重载该方法，在这个方法中，将每个Item的Gravity设置为CENTER
                 TextView textView = (TextView) super.getView(position, convertView, parent);
                 textView.setGravity(Gravity.CENTER);
                 return textView;
             }
         };
+        //4.将适配器关联到ListView
         listView.setAdapter(adapter);
+        /*
+         * 顶部和底部添加分割线,美观
+         * */
         listView.addHeaderView(new ViewStub(this));//顶部分割线
         listView.addFooterView(new ViewStub(this));//底部分割线
+
 
         //设置listview点击事件
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String param = data[position - 1];
-                switch (position) {
-                    case 1:
-                        func_1(param);
-                        break;
-                    case 2:
-                        func_2(param);
-                        break;
-                    case 3:
-                        func_3(param);
-                        break;
-                    case 4:
-                        func_4(param);
-                        break;
-                    case 5:
-                        func_5(param);
-                        break;
-                    case 6:
-                        func_6(param);
-                        break;
-                    case 7:
-                        func_7(param);
-                        break;
-                    case 8:
-                        func_8(param);
-                        break;
-                    case 9:
-                        func_9(param);
-                        break;
-                    case 10:
-                        func_10(param);
-                        break;
-                    case 11:
-                        func_11(param);
-                        break;
-                    case 12:
-                        func_12(param);
-                        break;
+                System.out.println("position : " + position + " id : " + id);
+                List<List_info> list_info = jsonRootBean.getList_info();
+                List_info info = list_info.get(position - 1);
+                show_board.setText(info.getFunc_name());
+                //获取Class对象
+                try {
+                    Class c = Class.forName("com.my.basedemo1.Handle");
+                    Object obj = c.newInstance();
+                    Method method = c.getMethod(info.getFunc_name(), null);
+                    method.invoke(obj);
+                } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                    e.printStackTrace();
                 }
             }
         });
